b17b46b017d9e42f07232976887abe876cb20cc8
Saravanan Padmanaban <saranbabu@hotmail.com>
2015-06-17 15:38:49 +0530

    Coding Stage
    Coding Stage Completed

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	# Initialize 
+	inverseincache <- NULL
+	matrixincache <- NULL
+
+
+	setmatrix <- function ( y )
+	{
+		x <<- y 
+		inverseincache <<- NULL
+		matrixincache <<- y
+	}
+
+	getmatrix <- function () x
+
+	setinversematrix <- function (solve) inverseincache <<-solve ## solve returns the inverse of matrix passed 
+
+	getinversematrix <- function () inverseincache ## returns the valueincache
+
+	list ( setmatrix=setmatrix, getmatrix=getmatrix, setinversematrix=setinversematrix, getinversematrix=getinversematrix )
+	
 }
 
 
@@ -12,4 +32,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inverseincache <- x$getinversematrix()
+		print("matrix in cache")
+		print(matrixincache)
+		print("get matrix")
+		print(x$getmatrix())
+		if ( identical( matrixincache, x$getmatrix() ) && !is.null(inverseincache) )
+		{ 
+			message ( "getting cached data" )
+			return(inverseincache)
+		}
+		else
+		{
+			sm<-x$getmatrix()
+			message("performing inverse ")
+			inverseincache<-solve(sm,...)
+			matrixincache<<-sm
+			x$setinversematrix(inverseincache)
+			inverseincache
+		}
 }
